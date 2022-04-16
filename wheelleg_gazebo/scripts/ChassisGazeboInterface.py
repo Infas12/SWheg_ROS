e250321a99b6260759dabb0a9c851bf4c4a7644d
infas12 <tiger12991@163.com>
2022-04-16 09:35:12 -0700

    added control mode change

---
+++ wheelleg_gazebo/scripts/ChassisGazeboInterface.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+from operator import mod
+import rospy
+from sensor_msgs.msg import Joy
+from legWheelController import LegWheelController , WheelJointController, JointManager
+from std_msgs.msg import Float64
+from controller_manager_msgs.srv import SwitchController
+
+def JoystickCallback(data):
+    
+    if(data.buttons[1]==1 and 
+       (JoystickCallback.prevdata is None or JoystickCallback.prevdata.buttons[1]!=1)
+       ):
+        JoystickCallback.isLegged = not JoystickCallback.isLegged
+        
+    JoystickCallback.prevdata = data
+
+
+
+if __name__ == '__main__':    
+    
+
+    rospy.init_node('chassisController', anonymous=True)
+    r = rospy.Rate(100)
+    legWheelController = LegWheelController()
+    
+    #wait for switch controller
+    rospy.wait_for_service('/WheelLeg/controller_manager/switch_controller')
+    switchServiceProxy = rospy.ServiceProxy('/WheelLeg/controller_manager/switch_controller', SwitchController)
+    
+    #test stuff
+    #initialize callback
+    JoystickCallback.isLegged = False
+    JoystickCallback.prevdata = None
+    joySub = rospy.Subscriber('joy',Joy,JoystickCallback)
+
+
+    Vy = 0.0
+    Vw = 0.0
+
+    left = ["LF","LM","LB"]
+    right = ["RF","RM","RB"]
+    global left_joint, right_joint
+    left_joint  = []
+    right_joint = []
+    for name in left:
+        left_joint.append(
+            WheelJointController(name=name,mode=0)
+        )        
+    for name in right:
+        right_joint.append(
+            WheelJointController(name=name,mode=0)
+        )       
+    
+    
+    while not rospy.is_shutdown():
+        
+        # update joystick readings
+        if(JoystickCallback.prevdata is not None):
+            legWheelController.isLegged = JoystickCallback.isLegged
+            Vy = 25.0 * JoystickCallback.prevdata.axes[0]
+            Vw = -25.0 * JoystickCallback.prevdata.axes[1]
+
+        if JoystickCallback.isLegged:
+            for joint in left_joint:
+                joint.setMode(1)
+            for joint in right_joint:
+                joint.setMode(1)
+        else:
+            for joint in left_joint:
+                joint.setMode(0)
+            for joint in right_joint:
+                joint.setMode(0)            
+
+        # control joints
+        for joint in left_joint:
+            joint.speedSet = Vy + Vw
+        for joint in right_joint:
+            joint.speedSet = Vy - Vw
+        
+
+        JointManager.instance().SendCommand()
+        
+        
+        r.sleep()
+        
---
+++ wheelleg_gazebo/scripts/legWheelController.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+import imp
+from tokenize import single_quoted
+import rospy
+from std_msgs.msg import Float64
+from Singleton import Singleton
+from controller_manager_msgs.srv import SwitchController
+
+@Singleton
+class JointManager:
+    def __init__(self):
+        self.controllerList = []
+    
+    def SendCommand(self):
+        for controller in self.controllerList:
+            controller.SendCommand()
+
+
+
+class LegWheelController:
+    
+    def __init__(self):
+        self.name_list = ["LFRA","LMRA","LBRA","RFRA","RMRA","RBRA",
+                 "LFRB","LMRB","LBRB","RFRB","RMRB","RBRB"]
+        self.transformer_joint_publisher_list = []
+        
+        for name in self.name_list:
+            self.transformer_joint_publisher_list.append(
+                rospy.Publisher('/WheelLeg/' + name +'_position_controller/command',Float64,queue_size=10)
+            )
+        
+        self.isLegged = False
+        self.joint_angle = 0.0
+        JointManager.instance().controllerList.append(self)
+        
+        
+    def SendCommand(self):
+        
+        if self.isLegged :
+            self.joint_angle = -0.5
+        else :
+            self.joint_angle = -0.0
+        
+        # send transformer joint command
+        for pub in self.transformer_joint_publisher_list:
+            pub.publish(self.joint_angle)
+            
+class WheelJointController:
+    
+    def __init__(self,name,mode = 0):
+        
+        self.positionController = name + '_position_controller'
+        self.velocityController = name + '_velocity_controller'
+        
+        self.speedSet = 0.0
+        self.positionSet = 0.0
+        self.mode = mode #0 - speed mode 1 - vel mode
+        
+        self.speedCmdPub = rospy.Publisher(
+            '/WheelLeg/' + self.velocityController +'/command',
+            Float64,queue_size=10)
+        
+        self.positionCmdPub = rospy.Publisher(
+            '/WheelLeg/' + self.positionController +'/command',
+            Float64,queue_size=10)
+        
+        self.switchServiceProxy = rospy.ServiceProxy(
+            '/WheelLeg/controller_manager/switch_controller', SwitchController)
+        
+        JointManager.instance().controllerList.append(self)
+    
+    def setMode(self,mode):
+        
+        if self.mode == mode:
+            return
+        else:
+            self.mode = mode
+            
+        if mode == 0: # position tp velocity
+            self.switchServiceProxy([self.velocityController],[self.positionController],2,1,1)
+        else:         # position tp position
+            self.switchServiceProxy([self.positionController],[self.velocityController],2,1,1)
+            
+    def SendCommand(self):
+        
+        if self.mode == 0: #speed mode
+            self.speedCmdPub.publish(self.speedSet)
+        else:
+            self.positionCmdPub.publish(self.positionSet)
+        
