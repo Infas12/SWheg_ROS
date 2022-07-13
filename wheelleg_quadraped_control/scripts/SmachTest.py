f91c3654ec3446c3c200d67ca4080f8cb73e2f4e
infas12 <tiger12991@163.com>
2022-07-13 04:27:43 -0700

    added playground

--- wheelleg_quadraped_control/scripts/SmachTest.py
+++ wheelleg_quadraped_control/scripts/SmachTest.py
@@ -6,13 +6,19 @@ from States.LegState import LegState
 from States.TransitionState import TransitionState
 
 if __name__ == '__main__':
+    
     rospy.init_node("StateMachine")
+    
     sm = smach.StateMachine(outcomes=['exit'])
+
     with sm:
+        
         smach.StateMachine.add('WHEEL',WheelState(),
                                transitions={'Transform':'TRANSITION'})
+        
         smach.StateMachine.add('TRANSITION',TransitionState(),
                                transitions={'TransformCompleted':'LEG'})
+        
         smach.StateMachine.add('LEG',LegState(),
                                transitions={'Transform':'WHEEL'})       
         
--- wheelleg_quadraped_control/scripts/States/LegState.py
+++ wheelleg_quadraped_control/scripts/States/LegState.py
@@ -2,7 +2,7 @@
 from ast import Pass
 import rospy
 from States.Motor import MotorManager
-from RobotState import RobotState, ContorlMode
+from States.RobotState import RobotState, ContorlMode
 
 
 class LegState(RobotState):
@@ -12,7 +12,9 @@ class LegState(RobotState):
         self.motorControlMode = ContorlMode.POS_MODE
         self.IsLeggedMode = True 
         self.tick = 0
-        self.period = 2000 # gait cycle
+        self.period = 1500 # gait cycle
+        self.Vy = 0.0
+        self.Vw = 0.0
     
     def execute(self, userdata):
         
@@ -22,12 +24,32 @@ class LegState(RobotState):
         
         while(not self.stateChangeFlag):
             
-            self.tick += 1
             
-            MotorManager.instance().getMotor("LF_Joint").positionSet = -self.getPosTrot(self.tick + 0.5*self.period,self.period)
-            MotorManager.instance().getMotor("LB_Joint").positionSet = -self.getPosTrot(self.tick,self.period)
-            MotorManager.instance().getMotor("RF_Joint").positionSet =  self.getPosTrot(self.tick,self.period)
-            MotorManager.instance().getMotor("RB_Joint").positionSet =  self.getPosTrot(self.tick + 0.5*self.period,self.period)            
+            
+            if self.joyData is not None:
+                self.Vw = 70.0 * self.joyData.axes[0]  
+                self.Vy = 2.0 * self.joyData.axes[1]
+                self.Vy = min(1,self.Vy)
+                self.Vy = max(-1,self.Vy)
+            
+            self.tick += 1.0*self.Vy
+            
+            print(self.Vy)  
+            
+            ## trot
+            # MotorManager.instance().getMotor("LF_Joint").positionSet = -self.generate_position(self.period,1000+self.Vw,self.tick)
+            # MotorManager.instance().getMotor("LB_Joint").positionSet = -self.generate_position(self.period,1000+self.Vw,self.tick + int(0.5*self.period))
+            # MotorManager.instance().getMotor("RF_Joint").positionSet =  self.generate_position(self.period,1000-self.Vw,self.tick + int(0.5*self.period))
+            # MotorManager.instance().getMotor("RB_Joint").positionSet =  self.generate_position(self.period,1000-self.Vw,self.tick)            
+
+
+            ## walk
+            MotorManager.instance().getMotor("LF_Joint").positionSet = -self.generate_position(self.period,1200+self.Vw,self.tick)
+            MotorManager.instance().getMotor("LB_Joint").positionSet = -self.generate_position(self.period,1200+self.Vw,self.tick + int(0.5*self.period))
+            MotorManager.instance().getMotor("RF_Joint").positionSet =  self.generate_position(self.period,1200-self.Vw,self.tick + int(0.25*self.period))
+            MotorManager.instance().getMotor("RB_Joint").positionSet =  self.generate_position(self.period,1200-self.Vw,self.tick + int(0.75*self.period))            
+
+
         
             self.sendData()
             
@@ -36,24 +58,37 @@ class LegState(RobotState):
         return "Transform"
     
     
-    def getPosTrot(self,timestamp,period):
-        # this thing is written like shit
+    def generate_position(self,period,time_stance,timestamp):
         
         PI = 3.1415926
-        leaveGroundAngleThres = - PI/2.5
-        leaveGrooundTimeThres = 0.8
+        time_flight = period - time_stance
+
+        turns = int(timestamp / period)
+        x     = float(timestamp % period)
         
-        x = (timestamp % period)/float(period)
-        turns = int(timestamp/period)
+        theta_hit   = - (PI - 0.7)
+        theta_leave = - 0.4
+        
+        k_flight = float(theta_hit - theta_leave) / float(time_flight)
+        k_stance = float(- PI - theta_hit + theta_leave) / float(time_stance)
+        
+        t_leave  = theta_leave / float(k_stance)
+        t_hit    = t_leave + time_flight
+        
+        angle_in_one_period = 0
+        
+        if x <= t_leave:
+            angle_in_one_period = float(k_stance * x)
+        elif x > t_leave and x <= t_hit:
+            angle_in_one_period = theta_leave + (x - t_leave) * k_flight
+        else :
+            angle_in_one_period = theta_hit + (x - t_hit) * k_stance
+        
         y = 0
-        k1 = (leaveGroundAngleThres - 0)/(leaveGrooundTimeThres)
-        k2 = (- PI - leaveGroundAngleThres)/(1-leaveGrooundTimeThres)
         
-        if x < leaveGrooundTimeThres:
-            y = k1*x
-        else:
-            y = leaveGroundAngleThres + k2*(x-leaveGrooundTimeThres)
-        
-        y = (y - PI/2.0 + PI + turns * PI) % (2 * PI) - PI
-        
+        if turns % 2 == 0: #even
+            y = angle_in_one_period
+        else: #odd
+            y =  PI + angle_in_one_period
+         
         return y
