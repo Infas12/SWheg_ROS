698614fe6d438506cf74e447e02e1b143160e230
infas12 <tiger12991@163.com>
2022-11-08 00:30:07 +0800

    added wheelleg_real package

---
+++ readme.md
@@ -0,0 +1,19 @@
+Terminal1:
+```
+roscore
+```
+
+Terminal2:
+```
+rosrun joy joy_node
+```
+
+Terminal3:
+```
+rosrun wheelleg_real motorController
+```
+
+Terminal4:
+```
+rosrun wheelleg_quadraped_control SmachTest.py
+```
---
+++ wheelleg_real/src/socketCan2/inc/M3508.hpp
@@ -0,0 +1,56 @@
+#ifndef M3508_HPP
+#define M3508_HPP
+
+#include "CanManager.hpp"
+#include "Pid.hpp"
+#include "Motor.hpp"
+
+
+class M3508 : public CanMsgHandler, public Motor
+{
+private:
+    
+    //attribute
+    uint32_t canId;
+    const static float RawPos2Rad;
+    const static float RawRpm2Rps;
+    const static float PiDiv19;
+    const static float CurrentRatio;
+    
+    //Raw feedback
+    uint16_t ecd  = 0;
+    int16_t speed_rpm = 0;
+    int16_t  given_current = 0;
+    uint8_t temperate = 0;
+    
+    //updated
+    float rotorPosition = 0.0;
+    float rotorLastPosition = 0.0;
+    
+public:
+
+    MotorControlModeType controlMode = Motor::RELAX_MODE;
+    Pid pidSpeed;
+    Pid pidPosition;
+
+public:
+
+    M3508() : CanMsgHandler() , Motor()
+    {};
+
+    void Registration(int id){
+        this->CanMsgHandler::Registration(id);
+        canId = id;
+    };
+
+    virtual void HandleNewMsg(can_frame msg);
+    void Update();
+
+public:
+    uint16_t ecd1  = 0;
+
+};
+
+
+
+#endif
